3f6239f3163c2c7cfc25c7e780c8c00323c9cca5
Max Sills <sillsm@gmail.com>
2015-07-13 06:51:59 +0000

    Forwarding git config args to container

--- start_container.sh
+++ start_container.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 # Start bash shell in worker container.
-sudo docker run -i -t econix/worker:v1 /bin/bash
+
+# Prepare to forward git config of build env. to container.
+EMAIL="$(git config --global user.email)"
+NAME="$(git config --global user.name)"
+
+sudo docker run -i -t -p 8080:8080 econix/worker:v1 /bin/bash \
+-c "clear & git config --global user.email '${EMAIL}' & git config --global user.name '${NAME}' & /bin/bash"
